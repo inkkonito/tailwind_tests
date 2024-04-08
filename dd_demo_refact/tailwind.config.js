3bdff3d1aa85d0dd61d4592aaaa15667e2e4c3ff
inkkonito <pro.julienp@gmail.com>
2024-04-08 19:07:20 +0200

    replicate form in tailwind

---
+++ dd_demo_refact/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        ddblue: "#49bff9",
+        ddred: "#ec5a5f",
+        ddgreen: "#03dac6",
+        ddgrey: "#3d4751",
+        ddwhite: "#e9f2f4",
+      },
+    },
+  },
+  plugins: [],
+};
