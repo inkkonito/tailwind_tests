2ed77b86b3cefb061257d6e293862284be82e0d5
inkkonito <pro.julienp@gmail.com>
2024-04-10 17:22:16 +0200

    finalized them + responsive design

---
+++ burger_xpress/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#E7272D",
+        secondary: "#FBB217"
+      },
+      backgroundImage: {
+        "ardoise": "url('images/background-slate.jpg')",
+      },
+    },
+  },
+  plugins: [],
+};
